809b6d022242e9ce5c388fac14170fb426ac482e
MigasEu <migas_sca@hotmail.com>
2017-05-30 15:10:42 +0100

    Default login changed
    Default login changed to our needs, with client checks.

--- DjangoWebPath/app/forms.py
+++ DjangoWebPath/app/forms.py
@@ -5,14 +5,29 @@ Definition of forms.
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext_lazy as _
+import re
 
 class BootstrapAuthenticationForm(AuthenticationForm):
     """Authentication form which uses boostrap CSS."""
-    username = forms.CharField(max_length=254,
+    username = forms.CharField(min_length=3, max_length=12,
                                widget=forms.TextInput({
-                                   'class': 'form-control',
+                                   'class': 'form-control inp_login',
                                    'placeholder': 'User name'}))
-    password = forms.CharField(label=_("Password"),
+    password = forms.CharField(min_length=3, max_length=12,
+                               label=_("Password"),
                                widget=forms.PasswordInput({
-                                   'class': 'form-control',
+                                   'class': 'form-control inp_login',
                                    'placeholder':'Password'}))
+
+    def clean_username(self):
+        inp = self.cleaned_data['username']
+        if not re.match('^[A-Za-z0-9]{3,12}$', inp):
+            raise forms.ValidationError("Incorrect username (3-12 numbers and letters)")
+        return inp
+
+    def clean_password(self):
+        inp = self.cleaned_data['password']
+        if not re.match('^[A-Za-z0-9]{3,12}$', inp):
+            raise forms.ValidationError("Incorrect password (3-12 numbers and letters)")
+        return inp
+
