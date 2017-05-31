6d64f2d3b56896b877a5b7142593c54e984196f4
MigasEu <migas_sca@hotmail.com>
2017-05-31 23:35:25 +0100

    Play story done
    User can now play stories.

--- DjangoWebPath/app/views.py
+++ DjangoWebPath/app/views.py
@@ -6,6 +6,11 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
+from app.models import Story, Piece, DecisionPiece, ChoicePiece
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from model_utils.managers import InheritanceManager
+from django.core.exceptions import ObjectDoesNotExist
 
 def home(request):
     """Renders the home page."""
@@ -16,6 +21,41 @@ def home(request):
         {
             'title':'Home Page',
             'year':datetime.now().year,
+        }
+    )
+
+def playStory(request, storyId):
+    assert isinstance(request, HttpRequest)
+    story = get_object_or_404(Story, pk=storyId)
+    return render(
+        request,
+        'app/playStory.html',
+        {
+            'title':'Play '+story.title,
+            'year':datetime.now().year,
+            'story':story
+        }
+    )
+
+def getPiece(request, storyId, pieceId):
+    assert isinstance(request, HttpRequest)
+    try:
+        piece = Piece.objects.get_subclass(pk=pieceId)
+    except ObjectDoesNotExist:
+        raise Http404("No piece matches the given query.")
+
+    choices = None
+    if isinstance(piece, DecisionPiece):
+        choices = ChoicePiece.objects.filter(decision=piece.pk)
+
+    return render(
+        request,
+        'app/piece.html',
+        {
+            'title':'Piece '+str(piece.pk),
+            'year':datetime.now().year,
+            'piece':piece,
+            'choices':choices
         }
     )
 
